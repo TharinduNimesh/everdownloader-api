6a676caebadf7d25599864112c496a3e31cef349
Tharindu Nimesh <127307898+TharinduNimesh@users.noreply.github.com>
2024-07-28 02:04:42 +0530

    get video information & publically accessible songs

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 import express from "express";
 import { YtDlp } from "@yemreak/yt-dlp";
 import cors from "cors";
-import fs from "fs";
+import ytdl from "ytdl-core";
 
 const app = express();
-const config = { workdir: "./downloads" };
+const config = { workdir: "./public/downloads" };
 const ytDlp = new YtDlp(config);
+const PORT = 8000;
+const APP_URL = `http://localhost:${PORT}`;
 
 app.use(
   cors({
-    origin: ["https://evermuzic.me", "http://localhost:3000"],
+    // origin: ["https://evermuzic.me", "http://localhost:3000"],
+    origin: "*",
   })
 );
+app.use(express.static("public"));
 
-app.get("/api/download", async (req, res) => {
+app.get("/api/info", async (req, res) => {
   const videoId = req.query.videoId;
-  if (!videoId && videoId.trim() === "") {
+  if (!videoId || videoId.trim() === "") {
     return res.json(
       {
         message: "Invalid Query",
@@ -23,19 +27,74 @@ app.get("/api/download", async (req, res) => {
       404
     );
   }
+
   const videoURL = `https://www.youtube.com/watch?v=${videoId}`;
   try {
-    const path = await ytDlp.download({
+    const info = await ytdl.getBasicInfo(videoURL);
+    return res.json({
+      title: info.videoDetails.title,
+      thumbnail: info.videoDetails.thumbnails[0].url,
+      duration: info.videoDetails.lengthSeconds
+        ? parseInt(info.videoDetails.lengthSeconds)
+        : 0,
+      channel: info.videoDetails.author.name,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error getting video info");
+  }
+});
+
+app.get("/api/download", async (req, res) => {
+  const videoId = req.query.videoId;
+  if (!videoId || videoId.trim() === "") {
+    return res.json(
+      {
+        message: "Invalid Query",
+      },
+      404
+    );
+  }
+
+  const videoURL = `https://www.youtube.com/watch?v=${videoId}`;
+  try {
+    const isExist = await checkFileExists(videoId);
+
+    if (isExist) {
+      return res.json({
+        message: "File already exists",
+        url: `${APP_URL}/downloads/${videoId}.m4a`,
+      });
+    }
+
+    await ytDlp.download({
       url: videoURL,
       format: "ba",
     });
-    return res.download(path.toString());
+    return res.json({
+      message: "Downloaded successfully",
+      url: `${APP_URL}/downloads/${videoId}.m4a`,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send("Error downloading video");
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+async function checkFileExists(videoId) {
+  try {
+    const url = `${APP_URL}/downloads/${videoId}.m4a`;
+    const response = await fetch(url);
+    if (response.ok) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.error(`Error accessing file: ${error.message}`);
+    return false;
+  }
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${APP_URL}`);
 });
